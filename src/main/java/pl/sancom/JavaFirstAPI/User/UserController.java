acb73977197cec3de7668ddfc7cf8f0ecdb81972
Aleksander Krawczyk <aleksander.krawczyk@sancom.pl>
2021-10-23 02:58:47 +0200

    user service

--- src/main/java/pl/sancom/JavaFirstAPI/User/UserController.java
+++ src/main/java/pl/sancom/JavaFirstAPI/User/UserController.java
@@ -3,11 +3,16 @@ package pl.sancom.JavaFirstAPI.User;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping("/users")
 	public List<User> GetAllUsers() {
@@ -16,5 +21,10 @@ public class UserController {
 				new User(1, "Olek2", "Nowy wpis 2"),
 				new User(1, "Olek3", "Nowy wpis 3")
 				);
+	}
+	
+	@RequestMapping("/users/{id}")
+	public User getUserById(@PathVariable Integer id) {
+		return userService.getUserById(id);
 	}
 }
